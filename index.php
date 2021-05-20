4c0553daf2d8838d75d340a6a2628c9494f8d431
Kassim <kassim91000@gmail.com>
2021-05-20 10:23:41 +0200

    Ajout page d'accueil

--- index.php
+++ index.php
@@ -2,9 +2,27 @@
 <div class="alrazy-title" data-splitting>
     Génie Mécanique et Productique
 </div>
+<!-- Script qui génére l'animation -->
 <script src="https://unpkg.com/splitting@1.0.0/dist/splitting.js"></script>
 <script>
     Splitting();
 </script>
 </br>
+</br>
+</br>
+</br>
+</br>
+<div class="container pt-4">
+
+
+    <table class="accueil"> 
+        <tr>
+            <td class="presentation"> <a href="/gmp/presentation.php"><h3>Présentation</h3> </a></td> 
+            <td class="formations"> <a href="/gmp/lpmief.php"><h3>Formations</h3>  </a> </td>
+        </tr>
+        <tr>
+        <td colspan="2" class="espace"><a href="/gmp/etudiant.php">  <h3>Espace Personnel</h3></a>  </td> 
+        </tr>
+    </table>
+</div>
 <?php include('./footer.php'); ?>
