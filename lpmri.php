7fd0d0ebc2b8400bdeb21797a5c5795c4edc76ad
CostierLucas <lucas.costier@outlook.fr>
2021-06-01 15:54:29 +0200

    lp mri

--- lpmri.php
+++ lpmri.php
@@ -1,3 +1,28 @@
 <?php include('./header.php') ?>
-
+<div id="titre-licence" class="container-fluid d-flex align-items-center justify-content-center">
+    <div class="row">
+        <h3>Licence professionnelle - mécatronique, robotique - Mécatronique - Robotique Intelligence</h3>
+    </div>
+</div>
+<div class="container pt-4">
+    <h3>Présentation de la licence</h3>
+    <hr>
+    <p> Le contenu des différents modules permettent aux étudiants de se positionner par rapport à leurs projets personnels. En plus des activités des apprentis en entreprise, la formation reconnaît l’engagement étudiant dans le cadre de plusieurs projets réalisés durant la formation, et également par le Projet tuteuré. Les projets tuteurés sont réalisés en groupe d’étudiants formés de deux Licences professionnelles.</p>
+    <p>Le programme de la formation LP Métiers de l'industrie : mécatronique, robotique (MIMR) est réalisé autour d’un seul parcours Mécatronique - Robotique Intelligente (M.R.I). Il est élaboré en concertation avec les acteurs de la profession. Il comprend un ensemble d’unités d’enseignement cohérent avec ses objectifs.</p>
+</div>
+<div class="container pt-4">
+    <h3>Objectifs </h3>
+    <hr>
+    <p>Donner aux apprentis les compétences nécessaires dans le domaine de la conception et de la mise en œuvre des systèmes mécatroniques en intégrant les problématiques de l'éco-conception. La mécatronique joue un rôle primordial dans le milieu industriel, en effet, elle permet de contrôler et commander des systèmes complexes, de maîtriser les outils informatiques, de piloter des systèmes robotisés dans différents domaines, en particulier, dans l'industrie de l'automobile, de l'aéronautique et dans le domaine de la santé.</p>
+</div>
+<div class="container pt-4">
+    <h3>Poursuite d'études</h3>
+    <hr>
+    <p>L’objectif principal de la formation et en particulier le parcours MRI, est l’insertion professionnelle directe et rapide des diplômés. </p>
+</div>
+<div class="container pt-4">
+    <h3>Insertion professionnelle</h3>
+    <hr>
+    <p>Ce parcours étant nouveau, le taux d'insertion sera étudié annuellement avec le CFA et les services de l’UEVE.</p>
+</div>
 <?php include('./footer.php'); ?>
